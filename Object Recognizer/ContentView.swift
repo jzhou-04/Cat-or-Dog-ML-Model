7cb9459b81a087f675d1c03bd3911cb9e2585780
Jeffrey Zhou <zhoujeffrey@yahoo.com>
2022-08-13 15:17:15 -0500

    Added feature to select in image from photo library

--- Object Recognizer/ContentView.swift
+++ Object Recognizer/ContentView.swift
@@ -1,21 +1,65 @@
 //
-//  ContentView.swift
 //  Object Recognizer
 //
-//  Created by Jeffrey Zhou on 8/13/22.
+//  Created by Jeffrey Zhou on 8/12/22.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View
+{
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    var body: some View
+    {
+        NavigationView
+        {
+            VStack
+            {
+                Text("Hello World")
+                VStack
+                {
+                    ZStack
+                    {
+                        Rectangle().fill(.secondary)
+                        Text("Tap to select a photo")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    .onTapGesture(perform: selectImage)
+                }
+                .padding([.horizontal, .bottom])
+                .onChange(of: inputImage){ _ in loadImage() }
+                .sheet(isPresented: $showingImagePicker)
+                {
+                    ImagePicker(image: $inputImage)
+                }
+            }
+        }
+    }
+    
+    func selectImage()
+    {
+        showingImagePicker = true
+        
+    }
+    
+    func loadImage()
+    {
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
