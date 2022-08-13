856d31757f67e21d278f1a04086020821fadbfc4
Jeffrey Zhou <zhoujeffrey@yahoo.com>
2022-08-13 15:09:10 -0500

    Initial Commit

---
+++ Object Recognizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Object Recognizer
+//
+//  Created by Jeffrey Zhou on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Object Recognizer/Object_RecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Object_RecognizerApp.swift
+//  Object Recognizer
+//
+//  Created by Jeffrey Zhou on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Object_RecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
